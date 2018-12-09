8f01fd5376fdb0f569de1e9c87333acf099d409a
TristanGomez44 <tristan.gomez@etu.univ-nantes.fr>
2018-12-09 16:15:23 -0500

    Moved evaluation functions to the processResults.py script

--- code/train_val.py
+++ code/train_val.py
@@ -38,9 +38,11 @@ def one_epoch_train(model,optimizer,trainMatrix, epoch, args,lr):
     neg_log_proba = model(data)
 
     loss = neg_log_proba
+
     loss.backward(retain_graph=True)
 
     if args.optim == "LBFGS":
+
         def closure():
             optimizer.zero_grad()
             loss = model(data)
@@ -49,6 +51,7 @@ def one_epoch_train(model,optimizer,trainMatrix, epoch, args,lr):
         optimizer.step(closure)
         optimizer.zero_grad()
     elif args.optim == "NEWTON":
+
         hessDiagList,gradientList = computeHessDiagList(loss,model)
 
         annot_bias = model.annot_bias-lr*gradientList[0]/hessDiagList[0]
@@ -59,20 +62,11 @@ def one_epoch_train(model,optimizer,trainMatrix, epoch, args,lr):
         model.setParams(annot_bias,annot_incons,video_amb,video_scor)
 
     else:
+
         optimizer.step()
         optimizer.zero_grad()
 
-    train_loss = loss
-
-    #Writing the CSV files in the results/NameOfTheExperience/ folder
-    #writeCSV(args,epoch,neg_log_proba)
-    firstTrainBatch = False
-
-    torch.save(model.state_dict(), "../models/{}/model{}_epoch{}".format(args.exp_id,args.ind_id, epoch))
-
-    #print('\nEpoch {} Train set: Average loss: {:.4f},\n'.format(epoch,train_loss))
-
-    return train_loss
+    return loss
 def writeCSV(args,epoch, log_proba):
     '''Write the loss of a model in a csv file
 
@@ -116,51 +110,6 @@ def computeHessDiagList(loss, model):
 
     return hessDiagList,gradientList
 
-def computeConfInter(loss,model):
-
-    hessDiagList,gradList = computeHessDiagList(loss,model)
-    confInterList = []
-
-    for hessDiag in hessDiagList:
-
-        confInter = 1.96/torch.sqrt(hessDiag)
-        confInterList.append(confInter)
-
-    return confInterList
-
-def plotMLE(loss,model,exp_id,epoch,mos_mean,mos_std):
-
-    confInterList = computeConfInter(loss,model)
-
-    for i,key in enumerate(model.state_dict()):
-
-        if key == "video_scor":
-            plt.figure(i,figsize=(13,5))
-
-        else:
-            plt.figure(i)
-
-        plt.grid()
-        plt.xlabel("Individual")
-        plt.ylabel("MLE")
-
-        if model.state_dict()[key].is_cuda:
-            values = model.state_dict()[key].cpu().numpy()
-            yErrors = confInterList[i].cpu().detach().numpy()
-        else:
-            values = model.state_dict()[key].numpy()
-            yErrors = confInterList[i].detach().numpy()
-
-        if key =="video_amb":
-            values = np.abs(values)
-
-        plt.errorbar([i for i in range(len(values))],values, yerr=yErrors, fmt="*")
-
-        if key == "video_scor":
-            plt.errorbar([i+0.5 for i in range(len(values))],mos_mean, yerr=mos_std, fmt="*")
-
-        plt.savefig("../vis/{}/{}_epoch{}.png".format(exp_id,key,epoch))
-
 def get_OptimConstructor(optimStr,momentum):
     '''Return the apropriate constructor and keyword dictionnary for the choosen optimiser
     Args:
@@ -172,6 +121,7 @@ def get_OptimConstructor(optimStr,momentum):
     if optimStr == "GD":
         optimConst = optim.SGD
         kwargs= {'momentum': momentum}
+
     elif optimStr == "LBFGS":
         optimConst = optim.LBFGS
         kwargs = {"history_size":1000,"max_iter":40}
@@ -223,117 +173,6 @@ def train(model,optimConst,kwargs,trainSet, args):
     print("\tStopped at epoch ",epoch)
     return loss,epoch
 
-def computeBaselines(trainSet):
-    mos_mean,mos_std = modelBuilder.MOS(trainSet,sub_rej=False,z_score=False)
-    sr_mos_mean,sr_mos_std = modelBuilder.MOS(trainSet,sub_rej=True,z_score=False)
-    zs_sr_mos_mean,zs_sr_mos_std = modelBuilder.MOS(trainSet,sub_rej=True,z_score=True)
-
-    return mos_mean,sr_mos_mean,zs_sr_mos_mean,mos_std,sr_mos_std,zs_sr_mos_std
-
-def mean_std(model,loss):
-    mle_mean = model.video_scor
-    mle_std = 1.96/torch.sqrt(computeHessDiag(loss,mle_mean)[0])
-    return mle_mean,mle_std
-
-def RMSE(vec_ref,vec):
-
-    if not (type(vec) is np.ndarray):
-        if vec.is_cuda:
-            vec = vec.cpu()
-        vec = vec.detach().numpy()
-    if not (type(vec_ref) is np.ndarray):
-        if vec_ref.is_cuda:
-            vec_ref = vec_ref.cpu()
-        vec_ref = vec_ref.detach().numpy()
-
-    sq_sum = np.power(vec_ref-vec,2).sum()
-    #if sq_sum>500:
-    #    print(vec_ref-vec)
-
-    #print(sq_sum)
-    return np.sqrt(sq_sum/len(vec))
-
-def deteriorateData(trainSet,nb_annot,nb_corr):
-
-    data = trainSet.clone()
-    nb_annot = int(nb_annot)
-    nb_corr = int(nb_corr)
-
-    #Remove annotators
-    if nb_annot < data.size(1):
-        annotToRemove = random.sample(range(data.size(1)),data.size(1)-nb_annot)
-        data = modelBuilder.removeColumns(data,annotToRemove)
-
-    #Randomly scramble subject scores
-    if nb_corr > 0:
-        annotToScramble = random.sample(range(nb_annot),nb_corr)
-        data = modelBuilder.scrambleColumns(data,annotToScramble)
-
-    return data
-
-def robustness(model,trainSet,distorNbList,args,paramRange,paramName,corrKwargs,nbRep=100):
-
-    #Getting the contructor and the kwargs for the choosen optimizer
-    optimConst,kwargs = get_OptimConstructor(args.optim,args.momentum)
-
-    model = modelBuilder.modelMaker(int(args.annot_nb),len(trainSet),distorNbList)
-    loss,_ = train(model,optimConst,kwargs,trainSet, args)
-
-    mle_mean_ref,_ = mean_std(model,loss)
-    mos_mean_ref,sr_mean_ref,zs_sr_mean_ref = computeBaselines(trainSet)[:3]
-
-    valueNumber = max(paramRange)-min(paramRange)
-    mle_err = np.zeros((valueNumber,nbRep))
-    mos_err = np.zeros((valueNumber,nbRep))
-    sr_err = np.zeros((valueNumber,nbRep))
-    zs_sr_err = np.zeros((valueNumber,nbRep))
-
-    #print(args.erase_results)
-    if (not os.path.exists("../results/{}/mle_err_epoch{}.csv".format(args.exp_id,args.epochs))) or args.erase_results:
-        for i,paramValue in enumerate(paramRange):
-
-            if i%5==0:
-                print(paramName,":",paramValue,"/",max(paramRange))
-            for j in range(nbRep):
-
-                corrKwargs[paramName] = paramValue
-
-                #Randomly remove or scramble annotator scores
-                data = deteriorateData(trainSet,**corrKwargs)
-
-                model = modelBuilder.modelMaker(int(corrKwargs["nb_annot"]),len(data),distorNbList)
-                loss,_ = train(model,optimConst,kwargs,data, args)
-
-                mle_mean,_ = mean_std(model,loss)
-                mos_mean,sr_mean,zs_sr_mean = computeBaselines(data)[:3]
-
-                mle_err[i,j] = RMSE(mle_mean_ref,mle_mean)
-                mos_err[i,j] = RMSE(mos_mean_ref,mos_mean)
-                sr_err[i,j] = RMSE(sr_mean_ref,sr_mean)
-                zs_sr_err[i,j] = RMSE(zs_sr_mean_ref,zs_sr_mean)
-
-            #sys.exit(0)
-        np.savetxt("../results/{}/mle_err_epoch{}_model{}.csv".format(args.exp_id,args.epochs,args.ind_id),mle_err)
-        np.savetxt("../results/{}/mos_err_epoch{}_model{}.csv".format(args.exp_id,args.epochs,args.ind_id),mos_err)
-        np.savetxt("../results/{}/sr_err_epoch{}_model{}.csv".format(args.exp_id,args.epochs,args.ind_id),sr_err)
-        np.savetxt("../results/{}/zs_sr_err_epoch{}_model{}.csv".format(args.exp_id,args.epochs,args.ind_id),zs_sr_err)
-
-    else:
-        mle_err = np.genfromtxt("../results/{}/mle_err_epoch{}.csv".format(args.exp_id,args.epochs))
-        mos_err =  np.genfromtxt("../results/{}/mos_err_epoch{}.csv".format(args.exp_id,args.epochs))
-        sr_err = np.genfromtxt("../results/{}/sr_err_epoch{}.csv".format(args.exp_id,args.epochs))
-        zs_sr_err = np.genfromtxt("../results/{}/zs_sr_err_epoch{}.csv".format(args.exp_id,args.epochs))
-
-    plt.figure()
-    #rangeAnnot = np.array(rangeAnnot)
-    plt.errorbar(paramRange,mos_err.mean(axis=1), yerr=1.96*mos_err.std(axis=1)/np.sqrt(nbRep),label="MOS")
-    plt.errorbar(paramRange,sr_err.mean(axis=1), yerr=1.96*sr_err.std(axis=1)/np.sqrt(nbRep),label="SR-MOS")
-    plt.errorbar(paramRange,zs_sr_err.mean(axis=1), yerr=1.96*zs_sr_err.std(axis=1)/np.sqrt(nbRep),label="ZS-SR-MOS")
-    plt.errorbar(paramRange,mle_err.mean(axis=1), yerr=1.96*mle_err.std(axis=1)/np.sqrt(nbRep),label="MLE")
-    plt.legend()
-
-    plt.savefig("../vis/{}/robustness_epoch{}_model{}.png".format(args.exp_id,args.epochs,args.ind_id))
-
 def main(argv=None):
 
     #Getting arguments from config file and command line
@@ -349,7 +188,6 @@ def main(argv=None):
     torch.manual_seed(args.seed)
     random.seed(args.seed)
 
-    trainSet,distorNbList = load_data.loadData(args.dataset,int(args.annot_nb))
     if args.cuda:
         trainSet = trainSet.cuda()
 
@@ -364,16 +202,17 @@ def main(argv=None):
     if not (os.path.exists("../models/{}".format(args.exp_id))):
         os.makedirs("../models/{}".format(args.exp_id))
 
-    modelType = "mlp"
-
     #Write the arguments in a config file so the experiment can be re-run
-    argreader.writeConfigFile("../models/{}/{}{}.ini".format(args.exp_id,modelType,args.ind_id))
+    argreader.writeConfigFile("../models/{}/model{}.ini".format(args.exp_id,args.ind_id))
+
+    #Loading data
+    trainSet,distorNbList = load_data.loadData(args.dataset)
 
     #Building the model
-    model = modelBuilder.modelMaker(int(args.annot_nb),len(trainSet),distorNbList)
+    model = modelBuilder.modelMaker(trainSet.size(1),len(trainSet),distorNbList,args.poly_deg)
     if args.cuda:
         model = model.cuda()
-    torch.save(model.state_dict(), "../models/{}/{}{}_epoch0".format(args.exp_id,modelType,args.ind_id))
+    torch.save(model.state_dict(), "../models/{}/model{}_epoch0".format(args.exp_id,args.ind_id))
 
     #Getting the contructor and the kwargs for the choosen optimizer
     optimConst,kwargs = get_OptimConstructor(args.optim,args.momentum)
@@ -384,13 +223,10 @@ def main(argv=None):
     if type(args.lr) is float:
         args.lr = [args.lr]
 
-    paramRange = range(args.param_min,args.param_max)
-    corrKwargs = {"nb_annot":args.annot_nb,"nb_corr":0}
+    loss,epoch = train(model,optimConst,kwargs,trainSet, args)
+    #print(model.video_amb)
 
-    robustness(model,trainSet,distorNbList,args,paramRange,args.param_name,corrKwargs,args.nb_rep)
-    #loss,epoch = train(model,optimConst,kwargs,trainSet, args)
-    #mos_mean,mos_std = modelBuilder.MOS(trainSet,sub_rej=True,z_score=False)
-    #plotMLE(loss,model,args.exp_id,epoch,mos_mean,mos_std)
+    torch.save(model.state_dict(), "../models/{}/model{}_epoch{}".format(args.exp_id,args.ind_id,epoch))
 
 if __name__ == "__main__":
     main()
