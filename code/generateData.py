ba0e2dad288e34aba6617a5d6cce5379099bf0d0
TristanGomez44 <tristan.gomez@etu.univ-nantes.fr>
2019-04-15 18:59:28 +0200

    Generate data in a parralel way

--- code/generateData.py
+++ code/generateData.py
@@ -19,16 +19,15 @@ import sys
 from matplotlib.lines import Line2D
 import matplotlib.cm as cm
 import os
+
+import modelBuilder
 def main(argv=None):
 
     #Getting arguments from config file and command line
     #Building the arg reader
     argreader = ArgReader(argv)
 
-    argreader.parser.add_argument('--bias_dis', metavar='STD',type=str,default='Beta',help='The bias distribution. Can be \'Beta\' or \'Normal\'')
-
     argreader.parser.add_argument('--bias_std', metavar='STD',type=float,default=0.25,help='The standard deviation of the bias gaussian distribution. Ignored if bias are sampled from a beta distribution')
-    argreader.parser.add_argument('--bias_ampl', metavar='STD',type=float,default=2,help='The amplitude of the bias gaussian distribution. Ignored if bias are sampled from a normal distribution')
 
     argreader.parser.add_argument('--incons_alpha', metavar='STD',type=float,default=1,help='The alpha parameter of the inconsistency beta distribution')
     argreader.parser.add_argument('--incons_beta', metavar='STD',type=float,default=10,help='The beta parameter of the inconsistency beta distribution')
@@ -46,6 +45,11 @@ def main(argv=None):
 
     argreader.parser.add_argument('--init_from',metavar='DATASETNAME',type=str,help='A new dataset can be created by removing lines or columns from of previously created one. \
                                   This argument allow this and its value should be the name of the older dataset.')
+
+    argreader.parser.add_argument('--seed_mat',metavar='SEED',type=int,help='The seed to use to generate the score matrix. \
+                                    If unspecified, The RNG state will not be reset after generating parameters.')
+
+    argreader.parser.add_argument('--plot',action='store_true',help='To plot the generated dataset.')
 
     #Reading the comand line arg
     argreader.getRemainingArgs()
@@ -78,7 +82,9 @@ def main(argv=None):
 
         nb_videos = args.nb_video_per_content*args.nb_content
 
+        print("Generating parameters")
         trueScores = trueScoreDis.sample((nb_videos,)).double()
+        trueScores[-1] = 4.9
 
         diffs = diffDis.sample((args.nb_content,)).double()
         incons = inconsDis.sample((args.nb_annot,)).double()
@@ -91,174 +97,103 @@ def main(argv=None):
         trueScores = trueScores.sort()[0]
         bias = bias.sort(dim=0)[0]
 
-        scoreMat = torch.zeros((nb_videos,args.nb_annot))
+        if args.seed_mat:
+            torch.manual_seed(args.seed_mat)
+            random.seed(args.seed_mat)
+            np.random.seed(args.seed_mat)
 
-        meanList = []
+        distorNbList = [args.nb_video_per_content for i in range(args.nb_content)]
+        model = modelBuilder.modelMaker(args.nb_annot,nb_videos,distorNbList,args)
 
-        colors = cm.rainbow(np.linspace(0, 1, nb_videos))
+        print("Preparing tensors")
+        model.preprocessAndSet(trueScores,"trueScores",args.score_dis,args.truescores_tanh,args.bias_tanh,args.bias_ampl)
+        model.preprocessAndSet(bias,"bias",args.score_dis,args.truescores_tanh,args.bias_tanh,args.bias_ampl)
+        model.preprocessAndSet(incons,"incons",args.score_dis,args.truescores_tanh,args.bias_tanh,args.bias_ampl)
+        model.preprocessAndSet(diffs,"diffs",args.score_dis,args.truescores_tanh,args.bias_tanh,args.bias_ampl)
 
-        markers = [m for m, func in Line2D.markers.items() if func != 'nothing' and m not in Line2D.filled_markers]
-        if args.nb_annot == 5:
-            markers = [".",",","v","1","s"]
-        if len(markers) < args.nb_annot:
-            markers = ["" for i in range(args.nb_annot)]
-        else:
-            markers = markers[:args.nb_annot]
+        print("Sampling scores")
+        scoreDis = model.compScoreDis(x_is_cuda=False)
+        scoreMat = scoreDis.sample()
 
-        gridspec.GridSpec(3+nb_videos,1)
-        fontSize = 20
+        if args.sample_mode == "continuous":
+            scoreMat = betaDenormalize(scoreMat,args.score_min,args.score_max)
 
-        scores = np.arange(args.score_min,args.score_max+1)
-        fig = plt.figure(figsize=(10,11))
-        plt.subplots_adjust(hspace=1.5,wspace = 0)
-        #ax = fig.add_subplot((1+nb_videos)*100+1*10+1,figsize=(10,5))
-        densityAxis = plt.subplot2grid((3+nb_videos,1), (0,0), colspan=1, rowspan=3)
-        box = densityAxis.get_position()
-        densityAxis.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-        densityAxis.set_xlabel("Normalized continuous raw score",fontsize=fontSize)
-        densityAxis.set_ylabel("Density",fontsize=fontSize)
+        elif args.sample_mode == "clip":
+            ampl = args.score_max-args.score_min+1
+            scoreMat = torch.clamp(torch.ceil(ampl*scoreMat),args.score_min,args.score_max)
 
-        #Plot the legend
-        handlesVid = []
-        for i in range(nb_videos):
-            handlesVid += densityAxis.plot((0,0),marker='',color=colors[i],label=i)
-        legVid = plt.legend(handles=handlesVid, loc='right' ,title="Videos",fontsize=fontSize,bbox_to_anchor=(1.35, 0.5))
-        plt.setp(legVid.get_title(),fontsize=fontSize)
-        plt.gca().add_artist(legVid)
-        handlesAnnot = []
-        for j in range(args.nb_annot):
-            handlesAnnot += densityAxis.plot((0,0),marker=markers[j],color="black",label=j)
-        legAnnot = plt.legend(handles=handlesAnnot, loc='right' ,title="Annotators",fontsize=fontSize,bbox_to_anchor=(1.4, -0.7),markerscale=4)
-        plt.setp(legAnnot.get_title(),fontsize=fontSize)
-        plt.gca().add_artist(legAnnot)
+        if args.plot:
 
-        dx = 0.01
-        x_coord = torch.arange(0,1,dx)
-        plt.xlim(0,1)
+            print("Ploting the data generated")
+            colors = cm.rainbow(np.linspace(0, 1, nb_videos))
 
-        for i in range(nb_videos):
+            markers = [m for m, func in Line2D.markers.items() if func != 'nothing' and m not in Line2D.filled_markers]
+            if args.nb_annot == 5:
+                markers = [".",",","v","1","s"]
+            if len(markers) < args.nb_annot:
+                markers = ["" for i in range(args.nb_annot)]
+            else:
+                markers = markers[:args.nb_annot]
+
+            gridspec.GridSpec(3+nb_videos,1)
+            fontSize = 20
+
+            fig = plt.figure(figsize=(10,11))
+            plt.subplots_adjust(hspace=1.5,wspace = 0)
+            #ax = fig.add_subplot((1+nb_videos)*100+1*10+1,figsize=(10,5))
+            densityAxis = plt.subplot2grid((3+nb_videos,1), (0,0), colspan=1, rowspan=3)
+            box = densityAxis.get_position()
+            densityAxis.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+            densityAxis.set_xlabel("Normalized continuous raw score",fontsize=fontSize)
+            densityAxis.set_ylabel("Density",fontsize=fontSize)
+
+            #Plot the legend
+            handlesVid = []
+            for i in range(nb_videos):
+                handlesVid += densityAxis.plot((0,0),marker='',color=colors[i],label=i)
+            legVid = plt.legend(handles=handlesVid, loc='right' ,title="Videos",fontsize=fontSize,bbox_to_anchor=(1.35, 0.5))
+            plt.setp(legVid.get_title(),fontsize=fontSize)
+            plt.gca().add_artist(legVid)
+            handlesAnnot = []
             for j in range(args.nb_annot):
+                handlesAnnot += densityAxis.plot((0,0),marker=markers[j],color="black",label=j)
+            legAnnot = plt.legend(handles=handlesAnnot, loc='right' ,title="Annotators",fontsize=fontSize,bbox_to_anchor=(1.4, -0.7),markerscale=4)
+            plt.setp(legAnnot.get_title(),fontsize=fontSize)
+            plt.gca().add_artist(legAnnot)
 
-                mean = trueScores[i]+bias[j]
-                mean = torch.clamp(mean,args.score_min+0.001,args.score_max-0.001)
-
-                if args.extr_sco_dep:
-                    #Add a dependency between the variance and the mean of videos.
-                    #Raw score variance of videos with extreme true scores will be lower (extreme means very high or very low).
-                    var = torch.pow(diffs[i//args.nb_video_per_content],2)*(-(trueScores[i]-args.score_min)*(trueScores[i]-args.score_max))
-                    var += torch.pow(incons[j],2)*(-(mean.item()-args.score_min)*(mean.item()-args.score_max))
-
-                else:
-                    var = torch.pow(diffs[i//args.nb_video_per_content],2)+torch.pow(incons[j],2)
-
-                mean = betaNormalize(mean,args.score_min,args.score_max)
-                var = betaNormalize(var,args.score_min,args.score_max,variance=True)
-                #The variance of the beta distribution can not be too big
-                var = torch.clamp(var,0.00001,mean.item()*(1-mean.item())-0.0001)
-
-                postProcessingFunc = lambda x:betaDenormalize(x,args.score_min,args.score_max)
-
-                if args.score_dis == "Beta":
-                    alpha,beta = meanvar_to_alphabeta(mean,var/args.div_beta_var)
-                    scoresDis = Beta(alpha,beta)
-                elif args.score_dis == "Normal":
-                    scoresDis = Normal(mean,var)
-                else:
-                    raise ValueError("Unkown distribution : {}".format(scoreDis))
-
-                '''
-                if args.score_dis == "Beta":
-                    mean = betaNormalize(mean,args.score_min,args.score_max)
-                    var = betaNormalize(var,args.score_min,args.score_max,variance=True)
-                    alpha,beta = meanvar_to_alphabeta(mean,var/args.div_beta_var)
-
-                    scoresDis = Beta(alpha,beta)
-                    postProcessingFunc = lambda x:betaDenormalize(x,args.score_min,args.score_max)
-                elif args.score_dis == "Normal":
-                    scoresDis = Normal(trueScores[i]+bias[j], diffs[i//args.nb_video_per_content]+incons[j])
-                    postProcessingFunc = lambda x:identity(x,args.score_min,args.score_max)
-                else:
-                    raise ValueError("Unkown distribution : {}".format(scoreDis))
-                '''
-
-                if args.sample_mode == "multinomial":
-                    probs = torch.zeros((args.score_max+1-args.score_min))
-
-                    for k in range(args.score_min,args.score_max+1):
-                        if args.score_dis == "Beta":
-                            value = torch.tensor(betaNormalize(k,args.score_min,args.score_max)).float()
-                            probs[k-1] = torch.exp(scoresDis.log_prob(value))
-                        else:
-                            probs[k-1] = torch.exp(scoresDis.log_prob(k))
-
-                    discDis = Multinomial(probs=probs)
-                    score = (discDis.sample().max(0)[1]+1).float()
-
-                    if args.score_dis == "Beta":
-                        scoreMat[i,j] = betaNormalize(score,args.score_min,args.score_max)
-                    else:
-                        scoreMat[i,j] = score
-
-                elif args.sample_mode == "continuous":
-                    scoreMat[i,j] = scoresDis.sample()
-
-                elif args.sample_mode == "clip":
-
-                    ampl = args.score_max-args.score_min+1
-
-                    if args.score_dis == "Beta":
-
-                        smpl = scoresDis.sample()
-                        smpl = torch.tensor([np.random.beta(alpha.item(), beta.item(), size=1)])
-
-                        #The sample method for the beta distribution has numerical instabilities and is bugged
-                        #which is why the sample is repeated until a good value is found
-                        while torch.isnan(smpl):
-                            smpl = scoresDis.sample()
-
-                        scoreMat[i,j] = torch.clamp(torch.ceil(ampl*smpl),args.score_min,args.score_max)
-                        postProcessingFunc = lambda x: x
-
-                    elif args.score_dis == "Normal":
-
-                        scoreMat[i,j] = torch.clamp(torch.ceil(ampl*scoresDis.sample()),args.score_min,args.score_max)
-                    postProcessingFunc = lambda x: x
-                else:
-                    print("Unknown sample mode : ",args.sample_mode)
-                    sys.exit(0)
-
-                cdf = lambda x: torch.exp(scoresDis.log_prob(x.double()))
-
-                densityAxis.plot(x_coord.numpy(),cdf(x_coord).cpu().detach().numpy(),color=colors[i],marker=markers[j])
-                #densityAxis.set_xticks(betaNormalize(scores,args.score_min,args.score_max))
-                #densityAxis.set_xticklabels(scores)
-                densityAxis.set_ylim(0,20)
-
-                meanList.append(mean)
-
-                scoreMat[i,j] = postProcessingFunc(scoreMat[i,j])
-
-            #Plot score histograms
-            #axHist = fig.add_subplot((1+nb_videos)*100+1*10+2+i)
-            scoreNorm = (scoreMat[i] - args.score_min)/(args.score_max+1-args.score_min)
-            histAx = plt.subplot2grid((3+nb_videos,1), (3+i,0), colspan=1, rowspan=1)
-
-            if i  == nb_videos-1:
-                histAx.set_xlabel("Raw scores",fontsize=fontSize)
-            if i == int(nb_videos//2):
-                histAx.set_ylabel("Empirical count",fontsize=fontSize)
-            #histAx.set_title("Video "+str(i))
-            box = histAx.get_position()
-            histAx.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-
-            plt.hist(scoreNorm,color=colors[i],width=1/(args.score_max-args.score_min+1),range=(0,1))
+            dx = 0.01
+            x_coord = torch.arange(0,1,dx)
             plt.xlim(0,1)
-            plt.ylim(0,args.nb_annot+1)
 
-            plt.xticks(betaNormalize(scores,args.score_min,args.score_max,rawScore=True),scores)
+            cdfs = torch.exp(scoreDis.log_prob(x_coord.unsqueeze(1).unsqueeze(1).double()))
 
+            for i in range(nb_videos):
+                for j in range(args.nb_annot):
 
-        plt.savefig("../vis/{}_scoreDis.png".format(args.dataset_id))
+                    densityAxis.plot(x_coord.numpy(),cdfs[:,i,j].cpu().detach().numpy(),color=colors[i],marker=markers[j])
+                    densityAxis.set_ylim(0,20)
+
+                #Plot score histograms
+                #scoreNorm = betaNormalize(scoreMat[i],args.score_min,args.score_max)
+                scoreNorm = (scoreMat[i] - args.score_min)/(args.score_max+1-args.score_min)+1/(2*(args.score_max+1-args.score_min))
+
+                histAx = plt.subplot2grid((3+nb_videos,1), (3+i,0), colspan=1, rowspan=1)
+
+                if i  == nb_videos-1:
+                    histAx.set_xlabel("Raw scores",fontsize=fontSize)
+                if i == int(nb_videos//2):
+                    histAx.set_ylabel("Empirical count",fontsize=fontSize)
+
+                box = histAx.get_position()
+                histAx.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+
+                plt.hist(scoreNorm,color=colors[i],width=1/(args.score_max-args.score_min+1),range=(0,1),bins=args.score_max-args.score_min+1)
+                plt.xlim(0,1)
+                plt.ylim(0,args.nb_annot+1)
+
+                scores = np.arange(args.score_min,args.score_max+1)
+                plt.xticks(betaNormalize(scores,args.score_min,args.score_max,rawScore=True),scores)
+            plt.savefig("../vis/{}_scoreDis.png".format(args.dataset_id))
 
         csv = "videos\tencode"+"".join(["\t{}".format(i) for i in range(args.nb_annot)])+"\n"
 
